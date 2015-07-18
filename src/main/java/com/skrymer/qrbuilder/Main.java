dd093854bc2121528d25f18b93e46f9bd60e4ace
skrymer <sonni.nielsen@gmail.com>
2015-07-18 17:13:34 +1200

    Updated example to use generics and the toFile() method

--- src/main/java/com/skrymer/qrbuilder/Main.java
+++ src/main/java/com/skrymer/qrbuilder/Main.java
@@ -10,19 +10,19 @@ import static com.skrymer.qrbuilder.decorator.ImageOverlay.*;
 import static com.skrymer.qrbuilder.decorator.ColoredQRCode.colorizeQRCode;
 
 public class Main {
-  public static void main(String[] args) {
-    try {
-      new ZXingQRCodeBuilder().newQRCode()
-                              .withSize(250, 250)
-                                .and()
-                              .withData("The answer to the universe and everything: 42")
-                                .and()
-                              .decorate(colorizeQRCode(Color.green.darker()))
-                                .and()
-                              .decorate(addImageOverlay(ImageIO.read(new File("src/test/resources/images/skull_bw.png")), 0.25f, 1f))
-                              .toFile("/home/skrymer/Desktop/qrcode.png", "PNG");
-    } catch (Exception e) {
-      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    public static void main(String[] args) throws Exception {
+        QRCBuilder<BufferedImage> qrCodeBuilder = new ZXingQRCodeBuilder();
+
+        qrCodeBuilder.newQRCode()
+        .withSize(250, 250)
+            .and()
+        .withData("The answer to the universe and everything: 42")
+            .and()
+        .decorate(colorizeQRCode(Color.green.darker()))
+            .and()
+        .decorate(addImageOverlay(ImageIO.read(new File("src/test/resources/images/skull_bw.png")), 0.25f, 1f))
+            .and()
+        .doVerify(true)
+        .toFile("./qrcode.png", "PNG");
     }
-  }
 }
